4b52dd0d856b44161030940d14795190b8110f23
Filipa Nunes <Filipa.Nunes@247tailorsteel.com>
2021-03-17 16:04:40 +0000

    add card game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,32 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { InitalMenuComponent } from './inital-menu/inital-menu.component';
+import { GameComponent } from './game/game.component';
+import { GameCardComponent } from './game-card/game-card.component';
+import { RestartDialogComponent } from './restart-dialog/restart-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InitalMenuComponent
+    InitalMenuComponent,
+    GameComponent,
+    GameCardComponent,
+    RestartDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
