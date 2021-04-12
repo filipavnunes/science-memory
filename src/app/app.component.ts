12d073f811f2a33181de4645e414d1d3245811b4
Filipa Nunes <Filipa.Nunes@247tailorsteel.com>
2021-04-12 19:29:23 +0100

    fix pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'science-memory';
+
+  updateChecked = false;
+  updateAvailable = false;
+  
+  get waitingForUpdates() {
+    return !this.updateChecked || this.updateAvailable;
+  }
+
+  constructor(private updates: SwUpdate) {}
+
+  async ngOnInit() {
+    this.updates.available.subscribe(() => {
+      this.updateAvailable = true;
+      window.location.reload();
+    });
+    if (this.updates.isEnabled) {
+      await this.updates.checkForUpdate();
+    } else {
+      console.log('Service worker updates are disabled.');
+    }
+    this.updateChecked = true;
+  }
 }
